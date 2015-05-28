4d00d4f213f407b42e115b3572d322f382a74d93
Tom Zhou <tomzhou94@gmail.com>
2015-05-28 13:30:47 -0700

    Added main.cpp
    Separated main() into a separated file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "my-router.h"
+#include <cstdlib>
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2 || strlen(argv[1]) != 1 
+		|| !valid_router_id(*(argv[1])))
+    {
+      std::cerr << "Usage: my-router <RouterID>\n";
+      return 1;
+    }
+
+    boost::asio::io_service io_service;
+    DVRouter B = DVRouter(*(argv[1]), io_service);
+    B.ft_print();
+    B.dv_print();
+}
