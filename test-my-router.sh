cb14060162b1aaef95c7558963d029c6f6eb8ef4
Tom Zhou <tomzhou94@gmail.com>
2015-05-30 14:28:40 -0700

    Start server in given order when using test script

--- test-my-router.sh
+++ test-my-router.sh
@@ -12,4 +12,17 @@ if [ "$#" -eq 1 ]; then
 			sleep 2
 		done
 	fi
+elif [ "$#" -eq 6 ]; then
+	for id in "$@"
+	do
+			if [ -f "routing-output$id.txt" ]; then
+				rm "routing-output$id.txt"
+			fi
+			gnome-terminal -x sh -c "./my-router $id"
+			sleep 2
+	done
+else
+	echo "Usage: "
+	echo "1) Start routers in random order: ./test-my-router.sh -r"
+	echo "2) Start routers in a given order: ./test-my-router.sh <sequence of IDs>"
 fi
