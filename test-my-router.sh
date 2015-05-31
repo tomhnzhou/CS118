a6af50ae3af0e637dee3acb4a1de9aa77cb040a6
Tom Zhou <tomzhou94@gmail.com>
2015-05-30 22:54:13 -0700

    Updated test script: parallel processes

--- test-my-router.sh
+++ test-my-router.sh
@@ -1,3 +1,8 @@
+print_err () {
+	echo "Usage: "
+	echo "1) Start routers in random order: ./test-my-router.sh -r"
+	echo "2) Start routers in a given order: ./test-my-router.sh <sequence of IDs>"
+}
 if [ "$#" -eq 1 ]; then
 	if [ "$1" == '-r' ]; then
 		randids=$(shuf -e A B C D E F)
@@ -8,9 +13,15 @@ if [ "$#" -eq 1 ]; then
 			if [ -f "routing-output$id.txt" ]; then
 				rm "routing-output$id.txt"
 			fi
-			gnome-terminal -x sh -c "./my-router $id"
-			sleep 2
+			if [ -f "routing-log$id.txt" ]; then
+				rm "routing-log$id.txt"
+			fi
+			./my-router "$id" &
+			sleep 2;
 		done
+		wait ${!}
+	else
+		print_err
 	fi
 elif [ "$#" -eq 6 ]; then
 	echo "Starting routers in this order: $1 $2 $3 $4 $5 $6"
@@ -19,11 +30,13 @@ elif [ "$#" -eq 6 ]; then
 			if [ -f "routing-output$id.txt" ]; then
 				rm "routing-output$id.txt"
 			fi
-			gnome-terminal -x sh -c "./my-router $id"
-			sleep 2
+			if [ -f "routing-log$id.txt" ]; then
+				rm "routing-log$id.txt"
+			fi
+			./my-router "$id" &
+			sleep 2;
 	done
+	wait ${!}
 else
-	echo "Usage: "
-	echo "1) Start routers in random order: ./test-my-router.sh -r"
-	echo "2) Start routers in a given order: ./test-my-router.sh <sequence of IDs>"
+	print_err
 fi
